82fcc2fbea26fcb3433d9d7e423e3be5200f9f34
arthurThorkildsen <arthurbt@stud.ntnu.no>
2025-02-06 13:00:41 +0100

    Added pdf icon

--- prototype.py
+++ prototype.py
@@ -3,10 +3,12 @@ import gradio as gr
 def callHtrFlow(image):
     return image
 
-def buttonCreator(text, style):
+def buttonCreator(text, style, path):
     button = gr.Button(
         value=text,
-        variant=[style]
+        variant=[style],
+        icon=path
+
     )
     return button
 
@@ -18,15 +20,15 @@ with gr.Blocks() as demo:
             input=gr.Image()
             # Bottom part of column
             with gr.Row():
-                cancel = buttonCreator("Cancel", "stop")
-                submit = buttonCreator("Submit", "secondary")
+                cancel = buttonCreator("Cancel", "stop", None)
+                submit = buttonCreator("Submit", "secondary", None)
         # Right column
         with gr.Column():
             output=gr.JSON()
             # Bottom part of column
             with gr.Row():
-                cancel = buttonCreator("Cancel", "stop")
-                submit = buttonCreator("Submit", "secondary")
-                PDF = buttonCreator("PDF", "huggingface")
+                cancel = buttonCreator("Cancel", "stop", None)
+                save = buttonCreator("Save", "secondary", None)
+                PDF = buttonCreator("PDF", "huggingface", "pdficon.svg")
 
 demo.launch()
