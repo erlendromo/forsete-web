1441c14bcd0426b32dc7dff7fd1e51684e64c96f
arthurThorkildsen <arthurbt@stud.ntnu.no>
2025-02-07 14:59:35 +0100

    Prototype successfully outputs text output in gui

--- prototype.py
+++ prototype.py
@@ -1,5 +1,6 @@
 import gradio as gr
 import subprocess
+import os
 
 
 def buttonCreator(text, style, path):
@@ -10,6 +11,19 @@ def buttonCreator(text, style, path):
     )
     return button
 
+# Reading and returning filepath to be used in output
+def formatImagePath(path):
+    # /example.jpg
+    filename = os.path.basename(path)
+    # removal of ".jpg"
+    filename, _ = os.path.splitext(filename)
+    # /dir
+    folder   = os.path.basename(os.path.dirname(path))
+    # /dir/example
+    last_two = os.path.join(folder, filename)
+    print(last_two)
+    return last_two
+
 def callHtrFlow(image_path):
     """
     Calls: htrflow pipeline <pipeline_path> <image_path>
@@ -17,6 +31,18 @@ def callHtrFlow(image_path):
     command = ["htrflow", "pipeline", "model/pipeline.yaml", image_path]
     print(f"Executing: {' '.join(command)}")
     subprocess.run(command, check=True)
+
+    # returning filepath to be used in output
+    filename_no_ext = formatImagePath(image_path)
+    txt_path = f"outputs/{filename_no_ext}.json"
+    print(txt_path)
+    # Reading the file
+    with open(txt_path, "r") as f:
+        text_output = f.read()
+        print(text_output)
+    
+    return text_output
+
 
 with gr.Blocks() as demo:
     gr.Markdown("# Prototype")
@@ -32,7 +58,7 @@ with gr.Blocks() as demo:
         # Right column
         with gr.Column():
             gr.Markdown("## Output") 
-            output=gr.JSON()
+            output=gr.Text()
             # Bottom part of column
             with gr.Row():
                 cancel = buttonCreator("Cancel", "stop", None)
