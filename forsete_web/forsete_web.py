96090d78a371b6d911af6ad2d36de97f9b6a7341
Arthur Borger Thorkildsen <arthur_bt@hotmail.no>
2025-03-04 11:46:40 +0100

    Added simple navbar #7

--- forsete_web/forsete_web.py
+++ forsete_web/forsete_web.py
@@ -14,6 +14,8 @@ class State(rx.State):
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
+        
+        navbar_searchbar(),
         rx.color_mode.button(position="top-right"),
         rx.vstack(
             rx.heading("Welcome to Reflex!", size="9"),
@@ -22,18 +24,61 @@ def index() -> rx.Component:
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
             spacing="5",
             justify="center",
             min_height="85vh",
         ),
-        rx.logo(),
     )
 
+def navbar_searchbar() -> rx.Component:
+    return rx.box(
+        rx.desktop_only(
+            rx.hstack(
+                rx.hstack(
+                    # Heading
+                    rx.heading(
+                        "FORSETE", size="7", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                # Search area
+                rx.input(
+                    rx.input.slot(rx.icon("search")),
+                    placeholder="Search...",
+                    type="search",
+                    size="2",
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        rx.mobile_and_tablet(
+            rx.hstack(
+                rx.hstack(
+                    rx.heading(
+                        "FORSETE", size="6", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.input(
+                    rx.input.slot(rx.icon("search")),
+                    placeholder="Search...",
+                    type="search",
+                    size="2",
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        bg=rx.color("accent", 3),
+        padding="1em",
+        # position="fixed",
+        # top="0px",
+        # z_index="5",
+        width="100%",
+    )
 
 app = rx.App()
 app.add_page(index)
