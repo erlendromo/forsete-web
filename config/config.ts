bbdddb5d2bcfff73cf0c7286233e0e244de6cb02
Arthur Borger Thorkildsen <arthur_bt@hotmail.no>
2025-04-10 10:49:56 +0200

    Added config file for configuring env. variables

--- config/config.ts
+++ config/config.ts
@@ -3,7 +3,8 @@ dotenv.config();
 
 const config = {
     urlBackend: process.env.URL_BACKEND || 'http://10.212.172.171:8080/forsete-atr/v1/',
-    port: parseInt(process.env.PORT || "3000")
+    port: parseInt(process.env.PORT || "3000"),
+    useMock: process.env.USE_MOCK === 'true'
 }
 
 export default config
