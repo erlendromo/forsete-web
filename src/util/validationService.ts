36cbfb65ec02d1b4e59f51bb959947b2e7ee72af
Arthur Borger Thorkildsen <arthur_bt@hotmail.no>
2025-04-30 22:25:35 +0200

    Added example for login page

---
+++ src/util/validationService.ts
@@ -0,0 +1,34 @@
+const MAX_MB =  32*1024**2; // 32MB
+const ALLOWED = [
+  "image/jpeg",
+  "image/png",
+  "image/tiff",
+  "application/pdf"
+] as const;
+
+/**
+ * Validates the file size, within the threshold set by "MAX_MB" or checks if the file 
+ * size is smaller than 0 or equal to 0.
+ * @param fileSize - The size of the file in bytes.
+ * @returns True if the file does not exceed the limit, false otherwise.
+ */
+export function validateFileSize(fileSize: number): boolean {
+    // Check if the file size is greater than 0 and less than or equal to MAX_MB
+  if (fileSize > MAX_MB || fileSize <= 0) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Validates the file type against a list of allowed MIME types.
+ * @param fileType - The MIME type of the file.
+ * 
+ * @returns True if the file type is valid, false otherwise.
+ */
+export function validateFileType(fileType: string): boolean {
+  if (!ALLOWED.includes(fileType as typeof ALLOWED[number])) {
+    return false;
+  }
+  return true;
+}
