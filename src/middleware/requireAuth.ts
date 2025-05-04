642c4e8aa89e0505223b1e59ccba944bd49cbf68
Arthur Borger Thorkildsen <arthur_bt@hotmail.no>
2025-05-04 16:46:48 +0200

    #31 Finished cookie, with correctly redirecting

--- src/middleware/requireAuth.ts
+++ src/middleware/requireAuth.ts
@@ -1,11 +1,16 @@
 // middleware/requireAuth.ts
 import { Request, Response, NextFunction } from 'express';
 import { getAuthToken } from '../util/cookieUtil.js';
+import { AppRoute } from '../config/constants.js';
 
 export function requireAuth(
   req: Request,
   res: Response,
+  next: NextFunction
 ) {
   const token = getAuthToken(req);
-  if (!token) return res.redirect('/login');
+  if (!token) {
+    return res.redirect(AppRoute.Login);
   }
+  next();
+}
