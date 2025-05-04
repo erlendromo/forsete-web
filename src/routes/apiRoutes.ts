dfd6e010213797f4fae2c09fc50b037fc448c3e7
Arthur Borger Thorkildsen <arthur_bt@hotmail.no>
2025-05-04 14:30:35 +0200

    #31 Refactored routes

---
+++ src/routes/apiRoutes.ts
@@ -0,0 +1,20 @@
+// src/routes/router.ts
+import { Router } from 'express';
+import { AppRoute } from '../config/constants.js';
+import { handleLogin, handleRegister } from '../services/userHandlingService.js';
+
+const router = Router();
+
+// Login route
+router.post(AppRoute.Login, (req, res) => {
+  const { email, password } = req.body;
+  return handleLogin(email, password, res);
+});
+
+// Register route
+router.post(AppRoute.Register, (req, res) => {
+  const { email, password } = req.body;
+  return handleRegister(email, password, res);
+});
+
+export default router;
---
+++ src/routes/renderingRoutes.ts
@@ -0,0 +1,25 @@
+// src/routes/renderingRoutes.ts
+import { Router } from 'express';
+import { config } from '../config/config.js';
+import { AppRoute, AppPages } from '../config/constants.js';
+import { requireAuth } from '../middleware/requireAuth.js';
+
+const router = Router();
+
+// Login route
+router.get(AppRoute.Login,    (_req, res) => res.render(AppPages.Login));
+// Register route
+router.get(AppRoute.Register, (_req, res) => res.render(AppPages.Register));
+
+// Protected routes
+// Home page route
+router.get(AppRoute.Home, requireAuth, (_req, res) => {
+  res.render(AppPages.Home, { config });
+});
+// Results page route
+router.get(AppRoute.Results, requireAuth, (req, res) => {
+  const fileName = req.query.file;
+  res.render(AppPages.Results, { fileName });
+});
+
+export default router;
