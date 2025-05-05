7c7cc7ea163e07c91d1fa14fa32c92794c24e6f0
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2025-05-05 16:49:40 +0200

    Refactored model/server.ts

--- src/model/server.ts
+++ src/model/server.ts
@@ -2,7 +2,6 @@
 import { config } from '../config/config.js';
 import express from "express";
 import path from "path";
-import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
 // Services
 import uploadRouter from "../services/index/uploadService.js";
@@ -19,34 +18,49 @@ const publicDir = "public";
 // Where html are stored
 const viewDir = "views";
 
-
+const setViews = () => {
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(process.cwd(), 'public/views'));
-
-
+}
 
 // Serves the public folder and views
-app.use(express.static(publicDir));
-app.use(express.static(publicDir+"/"+ viewDir));
-app.use(cookieParser());
-app.use(express.json());
-app.use(authRouter);
-app.use(renderRouter);
+const configureMiddlewares = () => {
+  app.use(express.static(publicDir));
+  app.use(express.static(path.join(publicDir, viewDir)));
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(authRouter);
+  app.use(renderRouter);
+  app.use(uploadRouter);
+  app.use(atrRouter);
+};
 
-const menuService = new MenuService(config, ApiEndpoints.MODELS_ENDPOINT);
-menuService.loadModelNames()
-  .then(modelsToMenu => { app.locals.modelNames = modelsToMenu; })
-  .catch(err => { console.error(err); process.exit(1); });
+const loadModels = async () => {
+  const menuService = new MenuService(config, ApiEndpoints.MODELS_ENDPOINT);
+  try {
+    const modelsToMenu = await menuService.loadModelNames();
+    app.locals.modelNames = modelsToMenu;
+  } catch (err) {
+    console.error('Error loading models:', err);
+    process.exit(1); // Terminate the server if model loading fails
+  }
+};
 
-// upload endpoint
-app.use(uploadRouter);
+const startServer = () => {
+  const documentation = 'forsete-atr/v2/swaggo/index.html';
+  app.listen(config.port, () => {
+    console.log(`Server running: http://localhost:${config.port}`);
+    console.log(`Backend documentation: ${config.urlBackend}${documentation}`);
+  });
+};
 
-// atr endpoint
-app.use(atrRouter);
+const setupServer = async () => {
+  setViews();
+  configureMiddlewares();
+  await loadModels();
+  startServer();
+};
 
-// Start server on port and log
-app.listen(config.port, () => {
-  console.log(`Server running: http://localhost:${config.port}`);
-});
+setupServer();
 
